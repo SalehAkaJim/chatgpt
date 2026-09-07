f964b46165a495785d11e0e02773feddbd0667b5
Jim <47636074+SalehAkaJim@users.noreply.github.com>
2026-09-07 08:54:22 +0400

    Nova: add v10 reset for semantic learning tables

---
+++ nova/database/reset_all_v10.sql
@@ -0,0 +1,21 @@
+-- NOVA v10 RESET (v9 core + semantic learning layer)
+SET FOREIGN_KEY_CHECKS=0;
+DROP VIEW IF EXISTS v_due_reviews;
+DROP VIEW IF EXISTS v_lesson_story;
+DROP TABLE IF EXISTS review_obligations;
+DROP TABLE IF EXISTS turn_learning_units;
+DROP TABLE IF EXISTS lesson_learning_units;
+DROP TABLE IF EXISTS learning_unit_words;
+DROP TABLE IF EXISTS curriculum_outcomes;
+DROP TABLE IF EXISTS learning_units;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS lesson_words;
+DROP TABLE IF EXISTS turns;
+DROP TABLE IF EXISTS words;
+DROP TABLE IF EXISTS lessons;
+DROP TABLE IF EXISTS characters;
+DROP TABLE IF EXISTS chapters;
+DROP TABLE IF EXISTS modules;
+DROP TABLE IF EXISTS levels;
+DROP TABLE IF EXISTS courses;
+SET FOREIGN_KEY_CHECKS=1;
